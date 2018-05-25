3a0eda4161bbc8d8cf95b4801cdbed7d884bf546
PrasanthSelvamani <prasanths2511@gmail.com>
2018-05-25 10:32:28 +0530

    first commit

---
+++ src/test/java/SampleTest/SampleTest.java
@@ -0,0 +1,29 @@
+package SampleTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void loginTest() throws IOException
+	{
+		WebDriver wd =new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		
+File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(src, new File("C:\\Users\\selenium\\eclipse-workspace\\Gittest\\Output\\googl.jpeg"));
+		
+	}
+
+	
+}
